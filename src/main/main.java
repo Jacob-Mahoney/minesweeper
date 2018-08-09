8014dd7f6a762993a8acc9374365cae84a5ba7f7
mjmj26 <mjmahoney26@gmail.com>
2018-08-08 21:55:51 -0500

    initial branch created

--- src/main/main.java
+++ src/main/main.java
@@ -1,11 +1,39 @@
 package main;
 
+import java.util.ArrayList;
+import java.util.List;
 public class main {
 
     public static void main(String args[]) {
 
-        
+      /*  int arr[] = {0,0,0,0,0,0,0,0,0,0};
+        int i = 0;
+        while (i<10){
+            int random = (int )(Math.random() * 81 + 1); //random number between 1 and 81
+            arr[i] = random;
+            System.out.println(arr[i]);
+            i++;
 
+        } */
+        ArrayList<ArrayList<Integer>> grid = new ArrayList<ArrayList<Integer>>();
+
+        int x = 9; // dummy variable dependent on user input for size selection (width)
+        int y = 9; // dummy variable dependent on user input for size selection (height)
+        int mines = 10;
+        while (mines>0) {
+            int random = (int )(Math.random() * (x) * (y) + 1); //random number between 1 and 81
+            arr[i] = random;
+            System.out.println(arr[i]);
+            mines--;
+        }
+
+        /*ArrayList<ArrayList<Square>> squares = new ArrayList<ArrayList<Square>>();
+        ArrayList<Square> firstRow = squares.get(0);
+        Square firstSquare = firstRow.get(0);
+        Square secondSquare = squares.get(1).get(1);
+        firstRow.set(5, new Square());
+         */
     }
+
 
 }
