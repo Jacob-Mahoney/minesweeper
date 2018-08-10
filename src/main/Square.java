a80e538f5e5dbd551f9a67c3a3f08955b39c5582
Jacob <mahoney.jacob@gmail.com>
2018-08-09 21:51:44 -0500

    added square class.

--- src/main/Square.java
+++ src/main/Square.java
@@ -2,6 +2,19 @@ package main;
 
 public class Square {
 
+    private int x, y;
+    private boolean hasMine;
 
+    Square(int x, int y, boolean hasMine) {
+        this.x = x;
+        this.y = y;
+        this.hasMine = hasMine;
+    }
+
+    int getX() { return x; }
+
+    int getY() { return y; }
+
+    boolean hasMine() { return hasMine; }
 
 }
