1b2d11579a9a56ee5ea05473784c11b39211ebb7
mjmj26 <mjmahoney26@gmail.com>
2018-08-09 21:57:54 -0500

    stuff

--- src/main/main.java
+++ src/main/main.java
@@ -15,15 +15,17 @@ public class main {
             i++;
 
         } */
-        ArrayList<ArrayList<Integer>> grid = new ArrayList<ArrayList<Integer>>();
-        ArrayList<Integer> firstRow = grid.get(0); //gets first row of grid
+        //ArrayList<ArrayList<Integer>> grid = new ArrayList<ArrayList<Integer>>();
+        //ArrayList<Integer> firstRow = grid.get(0); //gets first row of grid
+        ArrayList<Integer> firstRow = new ArrayList<Integer>();
         int x = 9; // dummy variable dependent on user input for size selection (width)
         int y = 9; // dummy variable dependent on user input for size selection (height)
         int mines = 10;
         while (mines>0) {
             int random = (int )(Math.random() * (x) * (y) + 1); //random number between 1 and 81
-            arr[i] = random;
-            System.out.println(arr[i]);
+            System.out.println(random);
+            firstRow.add(random);
+            System.out.println(firstRow);
             mines--;
         }
 
@@ -31,8 +33,7 @@ public class main {
         ArrayList<Square> firstRow = squares.get(0);
         Square firstSquare = firstRow.get(0);
         Square secondSquare = squares.get(1).get(1);
-        firstRow.set(5, new Square());
-         a*/
+        firstRow.set(5, new Square()); */
     }
 
 
